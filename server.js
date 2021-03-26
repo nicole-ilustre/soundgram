6a9ee039a74785514c5a94be20108f922b760519
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-03-26 14:32:54 +1300

    almost finished

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const hbs = require('express-handlebars')
 
@@ -11,16 +12,33 @@ server.use(express.urlencoded({ extended: false }))
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
-
 server.get('/', (req, res) => {
-    // res.send("Hello there!")
-    res.render('home')
+  res.render('home')
 })
 
 server.get('/result', (req, res) => {
-    
-    res.render('result')
+  res.render('result')
 })
 
+
+server.post('/result', (req, res) => {
+  fs.readFile('./sound.json', 'utf8', (err, data) => {
+    if (err) console.log(err)
+    const obj = JSON.parse(data)
+    const doorbell = obj.sounds.find(item => item.expected === 'doorbell')
+    doorbell.actual = req.body.answer1
+    const cat = obj.sounds.find(item => item.expected === 'cat')
+    cat.actual = req.body.answer2
+    const washingmachine = obj.sounds.find(item => item.expected === 'washingmachine')
+    washingmachine.actual = req.body.answer3
+    console.log(doorbell.actual)
+    console.log(cat.actual)
+    console.log(washingmachine.actual)
+    fs.writeFile('./sound.json', JSON.stringify(obj), (err) => {
+      if (err) console.log(err)
+      res.redirect('/result')
+    })
+  })
+})
 
 module.exports = server
